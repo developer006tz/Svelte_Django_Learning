0de87d3428f09d89ca2bae9dbf903a30938dbf9f
developer006 <developer.ludovic@gmail.com>
2023-11-12 12:38:17 +0300

    first commit

---
+++ frontend/src/routes/films/+page.svelte
@@ -0,0 +1,76 @@
+<script>
+    
+    let films = [
+        {id:1, name: 'The god father', year: 1972, genre: 'Crime, Drama', rating: 9.2, image: 'https://pictures-tanzania.jijistatic.com/3341817_MTA4MC0xMzUwLTk0NGZjNzUxMGM.webp'},
+        {id:2, name: 'The Shawshank Redemption', year: 1994, genre: 'Drama', rating: 9.3, image: 'https://pictures-tanzania.jijistatic.com/3341817_MTA4MC0xMzUwLTk0NGZjNzUxMGM.webp'},
+        {id:3, name: 'The Dark Knight', year: 2008, genre: 'Action, Crime, Drama', rating: 9.0, image: 'https://pictures-tanzania.jijistatic.com/3341817_MTA4MC0xMzUwLTk0NGZjNzUxMGM.webp'},
+        {id:4, name: 'SpiderMan 2', year: 2004, genre: 'Action, Adventure, Sci-Fi', rating: 7.3, image: 'https://pictures-tanzania.jijistatic.com/3341817_MTA4MC0xMzUwLTk0NGZjNzUxMGM.webp'},
+    ];
+
+</script>
+
+
+<ul>
+    {#each films as film}
+        <li>
+            <a href="films/{film.id}">
+                <!-- svelte-ignore a11y-img-redundant-alt -->
+                <img src={film.image} alt="film image">
+                <h2>{film.name}</h2>
+                <p>{film.year}</p>
+                <p>{film.genre}</p>
+                <p>{film.rating}</p>
+                
+            </a>
+        </li>
+    {/each}
+</ul>
+
+
+<style>
+    ul {
+        list-style: none;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-gap: 1rem;
+        grid-template-rows: auto;
+    }
+
+    li {
+        border: 1px solid #ccc;
+        padding: 1rem;
+        border-radius: 5px;
+    }
+
+    a {
+        text-decoration: none;
+        color: #333;
+    }
+
+    img {
+        max-width: 100%;
+        max-height: 100%;
+        object-fit: cover;
+    }
+
+    h2 {
+        margin: 0;
+        background: #333;
+        color: #fff;
+        padding: .5rem;
+    }
+    
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ way_clearing/settings.py
@@ -0,0 +1,142 @@
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-b_y2n1koq6c_!(kd$_czm+05tc-#hpv5j4++zi5@p)k4(i)+&7'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'core',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'way_clearing.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'way_clearing.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#connect to postgresql
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "options": "-c search_path=way"
+            
+        },
+        "NAME": env('DB_NAME'),
+        "USER": env('DB_USER'),
+        "PASSWORD": env('DB_PASSWORD'),
+        "HOST": env('DB_HOST'),
+        "PORT": env('DB_PORT'),
+
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
